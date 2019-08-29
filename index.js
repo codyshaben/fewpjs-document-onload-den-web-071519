e8fde2736f983e5ee75bea7d9c0fe1f553cfb1ce
Cody Shaben <codyshaben@Codys-MacBook-Air.local>
2019-08-28 22:36:51 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+// import { brotliDecompressSync } from "zlib";
+
 // Your code goes here
+document.addEventListener( "DOMContentLoaded", function () {
+    updateDOM()
+});
+
+function updateDOM() {
+    let p = document.querySelector('#text')
+        p.innerHTML = 'This is really cool!';
+}
+
+
+
+
+
